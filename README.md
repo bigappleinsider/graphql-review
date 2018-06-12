5fca24a7abcfa7cf81c75914a553d71710331cbb
Vlad Vinnikov <bigappleinsider@users.noreply.github.com>
2018-06-12 05:58:44 -0700

    query fragments, naming query, resolving nested data

---
+++ README.md
@@ -0,0 +1,89 @@
+### Naming a query
+
+```
+query fetchCompany{
+  company(id: "2") {
+    id
+    name
+    description
+    users {
+      id
+      firstName
+      age
+      company {
+        name
+        description
+      }
+    }
+  }
+}
+```
+
+### Renaming result
+- cannot have duplicate keys
+
+```
+query fetchCompany{
+  google: company(id: "2") {
+    id
+    name
+    description
+    users {
+      id
+      firstName
+      age
+      company {
+        name
+        description
+      }
+    }
+  }
+  apple: company(id: "1") {
+    id
+    name
+    description
+    users {
+      id
+      firstName
+      age
+      company {
+        name
+        description
+      }
+    }
+  }
+}
+
+```
+
+### Query fragments
+- a list of props we want to get access to
+- reduce duplication
+
+```
+fragment companyDetails on Company {
+  id
+  name
+  description
+}
+
+query fetchCompany{
+  google: company(id: "2") {
+   ...companyDetails
+  }
+  apple: company(id: "1") {
+    id
+    name
+    description
+    users {
+      id
+      firstName
+      age
+      company {
+        name
+        description
+      }
+    }
+  }
+}
+```
--- schema/schema.js
+++ schema/schema.js
@@ -5,22 +5,49 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
+/*
+running query:
+{
+  company(id: "2") {
+    id
+    name
+    description
+    users {
+      id
+      firstName
+      age
+      company {
+        name
+        description
+      }
+    }
+  }
+}
 
+*/
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: { 
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString }, 
     age: { type: GraphQLInt },
@@ -31,7 +58,7 @@ const UserType = new GraphQLObjectType({
           .then(res => res.data);
       } 
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -43,6 +70,14 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
           .then(res => res.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+          .then(res => res.data)
       }
     }
   }
